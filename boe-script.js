c6a2637bb0fa5501256d825d5ddb412547a1f14e
endanso1 <102243494+endanso1@users.noreply.github.com>
2025-04-10 09:50:53 +0000

    Add files via upload

---
+++ boe-script.js
@@ -0,0 +1,251 @@
+// Sample BOE data
+
+const boeData = {
+  40325155869: {
+    billNo: "AFO1-G-40325155869-01",
+    boe: "40325155869",
+    tin: "U2503140255",
+    payerName: "ALICE OWUSU",
+    amount: "3,447.09 GHS",
+    pdfDate: "14/03/2025 16:53",
+  },
+  40325136561: {
+    billNo: "AFO1-G-40325136561-01",
+    boe: "40325136561",
+    tin: "U2503140567",
+    payerName: "KWAME OSEI-OWUSU",
+    amount: "7,447.09 GHS",
+    pdfDate: "19/03/2025 10:53",
+  },
+  40325155874: {
+    billNo: "AFO1-G-40325155874-01",
+    boe: "40325155874",
+    tin: "U2503140258",
+    payerName: "ALICE OWUSU",
+    amount: "3,447.09 GHS",
+    pdfDate: "14/03/2025 17:06",
+  },
+  40325155893: {
+    billNo: "AFO1-G-40325155893-01",
+    boe: "40325155893",
+    tin: "U2503140271",
+    payerName: "ALICE OWUSU",
+    amount: "3,938.15 GHS",
+    pdfDate: "21/03/2025 16:03",
+  },
+  40325169231: {
+    billNo: "AFO1-G-40325169231-01",
+    boe: "40325169231",
+    tin: "U2503210253",
+    payerName: "GRACE OPOKU",
+    amount: "3,938.15 GHS",
+    pdfDate: "21/03/2025 16:03",
+  },
+  40325169246: {
+    billNo: "AFO1-G-40325169246-01",
+    boe: "40325169246",
+    tin: "U2503210259",
+    payerName: "GRACE OPOKU",
+    amount: "3,876.92 GHS",
+    pdfDate: "21/03/2025 16:10",
+  },
+  40325169257: {
+    billNo: "AFO1-G-40325169257-01",
+    boe: "40325169257",
+    tin: "U2503210261",
+    payerName: "GRACE OPOKU",
+    amount: "3,447.09 GHS",
+    pdfDate: "21/03/2025 16:16",
+  },
+  40325169270: {
+    billNo: "AFO1-G-40325169270-01",
+    boe: "40325169270",
+    tin: "U2503210273",
+    payerName: "GRACE OPOKU",
+    amount: "3,447.09 GHS",
+    pdfDate: "21/03/2025 16:23",
+  },
+  40325169285: {
+    billNo: "AFO1-G-40325169285-01",
+    boe: "40325169285",
+    tin: "U2503210296",
+    payerName: "GRACE OPOKU",
+    amount: "3,447.09 GHS",
+    pdfDate: "21/03/2025 16:28",
+  },
+  40325169303: {
+    billNo: "AFO1-G-40325169303-01",
+    boe: "40325169303",
+    tin: "U2503210299",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "21/03/2025 16:33",
+  },
+  40325170888: {
+    billNo: "AFO1-G-40325170888-01",
+    boe: "40325170888",
+    tin: "U250321223",
+    payerName: "GEORGE APPIAH",
+    amount: " 1,611.78 GHS",
+    pdfDate: "21/03/2025 18:04",
+  },
+  40325177383: {
+    billNo: "AFO1-G-40325177383-01",
+    boe: "40325177383",
+    tin: "U2503250203",
+    payerName: "EUNICE OUSMAN",
+    amount: " 820.18 GHS",
+    pdfDate: "25/03/2025 18:04",
+  },
+  40325185323: {
+    billNo: "AFO1-G-40325185323-01",
+    boe: "40325185323",
+    tin: "U2503280235",
+    payerName: "GRACE OPOKU",
+    amount: " 3,938.15 GHS",
+    pdfDate: "28/03/2025 16:05",
+  },
+  40325185334: {
+    billNo: "AFO1-G-40325185334-01",
+    boe: "40325185334",
+    tin: "U2503280247",
+    payerName: "GRACE OPOKU",
+    amount: " 3,876.92 GHS",
+    pdfDate: "28/03/2025 16:12",
+  },
+  40325185348: {
+    billNo: "AFO1-G-40325185348-01",
+    boe: "40325185348",
+    tin: "U2503280251",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "28/03/2025 16:18",
+  },
+  40325185356: {
+    billNo: "AFO1-G-40325185356-01",
+    boe: "40325185356",
+    tin: "U2503280257",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "28/03/2025 16:25",
+  },
+  40325185367: {
+    billNo: "AFO1-G-40325185367-01",
+    boe: "40325185367",
+    tin: "U2503280261",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "28/03/2025 16:34",
+  },
+  40325185385: {
+    billNo: "AFO1-G-40325185385-01",
+    boe: "40325185385",
+    tin: "U2503280273",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "28/03/2025 16:55",
+  },
+  40325184659: {
+    billNo: "AFO1-G-40325184659-01",
+    boe: "40325184659",
+    tin: "U2503280323",
+    payerName: "GEORGE APPIAH",
+    amount: " 1,048.78 GHS",
+    pdfDate: "28/03/2025 18:04",
+  },
+  40425194307: {
+    billNo: "AFO1-G-40425194307-01",
+    boe: "40425194307",
+    tin: "U2504040219",
+    payerName: "GRACE OPOKU",
+    amount: " 3,938.15 GHS",
+    pdfDate: "04/04/2025 16:08",
+  },
+  40425194318: {
+    billNo: "AFO1-G-40425194318-01",
+    boe: "40425194318",
+    tin: "U2504040228",
+    payerName: "GRACE OPOKU",
+    amount: " 3,876.92 GHS",
+    pdfDate: "04/04/2025 16:14",
+  },
+  40425194325: {
+    billNo: "AFO1-G-40425194325-01",
+    boe: "40425194325",
+    tin: "U2504040236",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "04/04/2025 16:18",
+  },
+  40425194337: {
+    billNo: "AFO1-G-40425194337-01",
+    boe: "40425194337",
+    tin: "U2504040245",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "04/04/2025 16:23",
+  },
+  40425194346: {
+    billNo: "AFO1-G-40425194346-01",
+    boe: "40425194346",
+    tin: "U2504040249",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "04/04/2025 16:30",
+  },
+  40425194359: {
+    billNo: "AFO1-G-40425194359-01",
+    boe: "40425194359",
+    tin: "U2504040266",
+    payerName: "GRACE OPOKU",
+    amount: " 3,447.09 GHS",
+    pdfDate: "04/04/2025 16:35",
+  },
+  40425193287: {
+    billNo: "AFO1-G-40425193287-01",
+    boe: "40425193287",
+    tin: "U2504030322",
+    payerName: "GEORGE APPIAH",
+    amount: " 989.14 GHS",
+    pdfDate: "04/04/2025 17:30",
+  },
+  40425194368: {
+    billNo: "AFO1-G-40425194368-01",
+    boe: "40425194368",
+    tin: "U2504040271",
+    payerName: "COMFORT KWARTENG",
+    amount: " 1,411.52 GHS",
+    pdfDate: "04/04/2025 16:40",
+  },
+};
+
+// Function to get query parameter from URL
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+// Load BOE data based on URL parameter
+function loadBOEData() {
+  const boeNumber = getQueryParam("boe_no");
+
+  // Convert boeNumber to a string for lookup
+  if (!boeNumber || !boeData[String(boeNumber)]) {
+    // alert("Invalid BOE Number! No record found.");
+    // return;
+    ("");
+  }
+
+  // Update BOE details
+  document.getElementById("bill-number").textContent =
+    boeData[boeNumber].billNo;
+  document.getElementById("boe-number").textContent = boeNumber;
+  document.getElementById("tin").textContent = boeData[boeNumber].tin;
+  document.getElementById("payer-name").textContent =
+    boeData[boeNumber].payerName;
+  document.getElementById("amount").textContent = boeData[boeNumber].amount;
+  document.getElementById("pdf-date").textContent = boeData[boeNumber].pdfDate;
+}
+
+// Run function on page load
+window.onload = loadBOEData;
